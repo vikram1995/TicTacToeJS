f55a38317a6b7324f465bd395d644414bd321519
vikran-obend <vikram.1rn14ec170@gmail.com>
2020-04-09 13:55:09 +0530

    changed the input method and added the win logic

--- script.js
+++ script.js
@@ -1,97 +1,159 @@
-let inputs = [];
-let len = 9;
-let full = false;
-let position;
-let option;
-
-let player = 1;
-let win =false;
-
-let count = 0;
-let emptyPositions;
-
-//console.log(inputs);
+var boxValues = [];
+var arrayOfPlayer1=[];
+var arrayOfPlayer2=[];
+var player = 1;
+var playerwin = false;
+var boxClicked;
+var boxClickedId;
+var box;
 
 
-searchEmptyPosition = () => {
-    emptyPositions="";
-    for(let i=0; i<len;i++)
+togglePlayer = () => {
+    
+    if(player == 1)
     {
-        if(inputs[i] == null )
-        {   
-
-            emptyPositions = emptyPositions+ " "  + (i+1);
-            
-        }
-    }
-    
-}
- takeInput  = () =>{
-    
-    searchEmptyPosition();
-    
-
-    let message = "player " + player + " " + "select the position out of - " + emptyPositions;
-    
-    position = prompt(message);
-
-
-    if(inputs[position-1]!=null)
-    {
-        alert("already occoupied");
-        count--;
-        return;
-    }
-
-    if(position == 1 ||position == 2 || position == 3 || position == 4 ||position == 5 ||position == 6 ||position == 7 ||position == 8 ||position == 9 )
-    {
-        if(player == 1)
-        {
-            option = "X"
-        }
-
-        else if(player == 2)
-        {
-            option = "O"
-        }
-            inputs[position-1] = option ;
-            console.log(inputs.length);
-            var pos = document.querySelector("#p-" + position);
-            pos.textContent = option;
-    
-
+        player = 2;
         
     }
 
-    else{
-        alert("invalid position try again !");
-        takeInput();
+    else if (player == 2)
+    {
+        player = 1;
+    }
+    playerTurnDisplay(player);
+}
+
+playerTurnDisplay = (player) => {
+    let dipElement = document.querySelector('.player-to-play');
+    dipElement.textContent = "Player " + player + " turns"
+}
+
+
+fillBox = () => {
+     box = document.querySelector('#' + boxClickedId);
+   
+    if(player == 1)
+    {
+        box.textContent = "X";
+        arrayOfPlayer1.push(boxClicked-1);
+        boxValues[boxClicked-1] = 1;
+    }
+
+    if(player == 2)
+    {
+        box.textContent = "O";
+        arrayOfPlayer2.push(boxClicked-1);
+        boxValues[boxClicked-1] = 2;
     }
 
     
     
- }
-
- 
-
- 
-while(!win && !full)
-{
-  takeInput();
-  if(player == 1)
-  {
-      player = 2;
-  }
-
-  else if(player == 2)
-  {
-      player = 1;
-  }
-
-  count++;
-  
-  if(count === 9)
-  {
-      full = true;
-  }
 }
+
+
+ 
+
+winCalculator = (player) => {
+
+    function checkForElement(item){
+        for(i=0;i<winArray.length;i++)
+        {
+            if(winArray[i][0]== item[0] && winArray[i][1]== item[1] && winArray[i][2]== item[2] 
+                || winArray[i][0]== item[1] && winArray[i][1]== item[2] && winArray[i][2]== item[3]
+                || winArray[i][0]== item[0] && winArray[i][1]== item[2] && winArray[i][2]== item[3]
+                || winArray[i][0]== item[0] && winArray[i][1]== item[1] && winArray[i][2]== item[3])
+            {
+                return true;
+            }
+        }
+    }
+    
+    function playerWinDisplay()
+    {
+            let win = document.querySelector('.player-win');
+            win.textContent = "Player " + player + " wins";
+            win.style.color = "green"
+            let dipElement = document.querySelector('.player-to-play');
+            dipElement.style.display = "none";
+    }
+
+    var winArray = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    
+    if(player == 1)
+    {
+        if(checkForElement(arrayOfPlayer1.sort()))
+        {
+            console.log("player 1 wins!");
+            playerWinDisplay();
+            
+            playerwin = true;
+        }
+        
+        
+    }
+
+    else if(player == 2)
+    {
+        if(checkForElement(arrayOfPlayer2.sort()))
+        {
+            console.log("player 2 wins!");
+            playerWinDisplay();
+            playerwin = true;
+        }
+    }
+
+}
+
+function isDraw(){
+
+}
+    
+(function gameControler()
+{
+    var error;
+    var count=9;
+    window.onclick = e => {
+        if(count >0 && !playerwin)
+        {
+            boxClickedId = e.target.id;
+            boxClicked = e.target.id.substring(2,3); 
+            console.log(boxClicked);
+            box = document.querySelector('#' + boxClickedId);
+            if(box.textContent != "X" && box.textContent !="O" || box.textContent == "")
+            {
+                
+                fillBox();
+                winCalculator(player);
+                togglePlayer();
+                count--;
+                if(count == 0)
+                {
+                    let win = document.querySelector('.player-win');
+                    win.textContent = "DRAW";
+                    win.style.color = "red";   
+                }
+            }
+    
+            else{
+                
+                error = document.querySelector('.error')
+                error.textContent = "already filled box choose another box";
+                
+            }
+        }
+
+        else{
+            if(!playerwin)
+            {
+                let win = document.querySelector('.player-win');
+                win.textContent = "DRAW";
+                win.style.color = "red";
+            }
+            
+        }
+       
+        
+    }
+
+
+})()
